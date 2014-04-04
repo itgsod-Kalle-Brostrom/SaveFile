24bb388724eacd61d7d744df755f2037ddc1296d
itg <kalle.brostrom@elev.it-gymnasiet.se>
2014-04-04 13:19:46 +0200

    Added alot of pro files,

---
+++ lib/terminal_saver.rb
@@ -0,0 +1,12 @@
+class TerminalSaver
+
+  def save(hash)
+    output = "#{hash[:type]}:\n"
+    output  <<   "width: {#hash[:width]}\n"
+    output  <<   "heigth: {#hash[:height]}\n"
+    output  <<   "area: {#hash[:area]}\n"
+    output  <<   "colour: {#hash[:colour]}\n"
+    puts output
+  end
+
+end
---
+++ spec/terminal_saver_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'stringio'
+require_relative '../lib/terminal_saver'
+
+describe 'saving a rectangle' do
+
+  before do
+    $stdout = StringIO.new
+  end
+
+  after(:all) do
+    $stdout = STDOUT
+  end
+
+  it 'should print the rectangle to the command prompt' do
+    ts = TerminalSaver.new
+    ts.save({type: 'Rectangle', width:4, height: 3, area: 12, colour: 'red'})
+    correct_output = "Rectangle:\n  width: 4\n  height: 3\n  area: 12\n  colour: red"
+    #STDOUT.should_receive(:print).with(correct_output)
+    expect($stdout.string).to match(correct_output)
+  end
+
+end
+
